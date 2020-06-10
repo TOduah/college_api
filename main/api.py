f0b52bb5d9ebf7ee6386b74b7adbb8eb9148df18
tobenna <tjlite81@gmail.com>
2020-06-09 23:41:40 -0400

    start of template addition

--- main/api.py
+++ main/api.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template 
 from flask import request, jsonify 
 
 app = Flask(__name__)
@@ -8,11 +8,18 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<p>Hello, world!</p>"
+    return render_template('index.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
 
 # @app.route('/api/v1/resources/books/all', methods=['GET'])
 # def api_all():
 #     return jsonify(books)
+
+# @app.route('/api/v1/resources/book/<string:name>', methods=['GET'])
+# def home(name):
 
 
 
