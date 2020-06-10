acc532b1de5f47368ea1cfb9bec3078baa4139dc
tobenna <tjlite81@gmail.com>
2020-06-09 23:28:21 -0400

    minor import changes

--- main/api.py
+++ main/api.py
@@ -1,14 +1,14 @@
-import flask 
+from flask import Flask 
 from flask import request, jsonify 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Hello, world!</p>"
+    return "<p>Hello, world!</p>"
 
 # @app.route('/api/v1/resources/books/all', methods=['GET'])
 # def api_all():
