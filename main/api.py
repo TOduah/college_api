a03fef6be8ca7c5da109e9e0671002e02a3fceb8
tobenna <tjlite81@gmail.com>
2020-06-14 07:30:38 -0400

    added code to get colleges that correspond to specific qualities

--- main/api.py
+++ main/api.py
@@ -29,6 +29,30 @@ def api_all():
 
     return jsonify(data)
 
+# endpoint for specific college
+@app.route('/api/v1/colleges/<string:college_name>', methods=['GET'])
+def get_college(college_name):
+    with open('data/collegesdata.json', 'r') as f:
+        data = json.load(f)
+
+    for college in data:
+        if college_name in college["college-name"]:
+            return jsonify(college)
+    return None
+
+# endpoint for colleges of a specific academic-calendar
+@app.route('/api/v1/colleges/calendar/<string:calendar>', methods=['GET'])
+def get_calendar(calendar):
+    with open('data/collegesdata.json', 'r') as f:
+        data = json.load(f)
+    c_list = []
+
+    for c in data:
+        if c["academic-calendar"] == calendar:
+            c_list.append(c)
+
+    return jsonify(c_list)
+
      
 
 # @app.route('/api/v1/resources/book/<string:name>', methods=['GET'])
